a5f26d0effa36529341041b9cd934d1187fbda66
Scola <a.pagano45@studenti.uniba.it>
2025-01-29 16:03:44 +0100

    modifiche navbar e css

--- frontend/src/Pages/App.js
+++ frontend/src/Pages/App.js
@@ -1,10 +1,10 @@
 import Navbar from '../Components/Navbar'
 import '../css/App.css'
 function App() {
-  const elem = [{name: "Home", fragment: "#home"}, {name: "Servizi", fragment: "#services"}, {name: "Chi siamo", fragment: "#aboutus"}, {name: "Contatti", fragment: "#contact"}, {name: "Accedi", fragment: "#signin"}];
+  const items = [{name: "Home", fragment: "#home"}, {name: "Servizi", fragment: "#services"}, {name: "Chi siamo", fragment: "#aboutus"}, {name: "Contatti", fragment: "#contact"}, {name: "Accedi", fragment: "#signin"}];
   return (
     <div className="App">
-      <Navbar items={elem}/>
+      <Navbar items={items}/>
       <p>asddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasddddddddddasdddddddddd<br/>
       asddddddddasdddddddddd<br/>
       asdddddddd</p>
