765c90a12236ffc2e8cd11a5f117847b482995b7
Scola <a.pagano45@studenti.uniba.it>
2025-01-30 17:45:11 +0100

    Modifica file di avvio automatica (aggiunta di npm install sia per frontend che per backend)

--- start.bat
+++ start.bat
@@ -5,10 +5,15 @@ set FRONTEND_PATH=.\frontend
 set BACKEND_PATH=.\backend
 set PROJECT_PATH=.\
 
-:: Apri terminale per React
-start cmd /k "cd /d %FRONTEND_PATH% && npm start"
+:: Avvia in parallelo la procedura per il frontend:
+:: 1) npm install
+:: 2) npm start
+start cmd /k "cd /d %FRONTEND_PATH% && npm install && npm start"
 
-:: Apri terminale per Express
-start cmd /k "cd /d %BACKEND_PATH% && node index.js"
+:: Avvia in parallelo la procedura per il backend:
+:: 1) npm install
+:: 2) node index.js
+start cmd /k "cd /d %BACKEND_PATH% && npm install && node index.js"
 
 echo Script avviato con successo!
+
